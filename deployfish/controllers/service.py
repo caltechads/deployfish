74baccae42f0597bc6b76883985b0aaf82e63ae1
Robert Rollins <rrollins@caltech.edu>
2024-07-15 11:56:42 -0700

    Fixed two printout bugs
    Looks like these two were accidentally written as click.style(), when they should have been click.secho(). It was causing them to not print anything.

--- deployfish/controllers/service.py
+++ deployfish/controllers/service.py
@@ -249,7 +249,7 @@ class ECSService(CrudBase):
         obj.scale(count)
         self.scale_services_waiter(obj)  # type: ignore
         self.app.print(
-            click.style('\n\nScaled {}("{}") to {} tasks.'.format(self.model.__name__, obj.pk, count), fg='green')
+            click.secho('\n\nScaled {}("{}") to {} tasks.'.format(self.model.__name__, obj.pk, count), fg='green')
         )
         for _ in self.app.hook.run('post_service_scale', self.app, obj, count):
             pass
@@ -275,7 +275,7 @@ class ECSService(CrudBase):
         obj = loader.get_object_from_aws(self.app.pargs.pk)
         obj = cast(Service, obj)
         obj.restart(hard=self.app.pargs.hard, waiter_hooks=[ECSDeploymentStatusWaiterHook(obj)])
-        return click.style('\n\nRestarted tasks for {}("{}").'.format(self.model.__name__, obj.pk), fg='green')
+        return click.secho('\n\nRestarted tasks for {}("{}").'.format(self.model.__name__, obj.pk), fg='green')
 
     @ex(
         help='List the running tasks for an ECS Service in AWS.',
@@ -339,7 +339,6 @@ class ECSServiceStandaloneTasks(Controller):
 
     model: Type[Model] = Service
     loader: Type[ObjectLoader] = ServiceLoader
-
 
     @ex(
         help='List StandaloneTasks related to a Service from configuration in deployfish.yml',
@@ -428,7 +427,6 @@ class ECSServiceStandaloneTasks(Controller):
             click.secho('\nDone.', fg='yellow')
         else:
             self.app.print('No related tasks.')
-
 
 
 class ECSServiceSecrets(ObjectSecretsController):
