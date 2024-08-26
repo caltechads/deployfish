605fca659023da902450fa9d1ad8d99fb82eef29
Glenn Bach <glenn@caltech.edu>
2024-08-26 16:58:05 -0700

    Slack service update notification plugin

---
+++ deployfish/plugins/slack/hooks.py
@@ -0,0 +1,129 @@
+import logging
+import os
+import getpass
+
+from slackfin import (
+    SlackFormatter,
+    SlackLabelValueListBlock,
+    SlackLabelValuePair,
+    SlackMarkdownType,
+    SlackMessageContext,
+    SlackMessageDivider,
+    SlackMessageHeader,
+    SlackMessage,
+    SlackMessageMarkdown,
+)
+
+#pylint: disable=no-name-in-module
+from deployfish.core.utils.mixins import (
+    PythonMixin,
+    GitMixin,
+    GitChangelogMixin,
+)
+
+from deployfish.core.models.ecs import Service
+
+logging.basicConfig(level=logging.WARNING)
+
+
+def process_service_update(app, obj, success=True, reason=None):
+    if not success:
+        return
+    if not isinstance(obj, Service):
+        return
+    config_file = app.pargs.deployfish_filename
+    repo_folder = os.path.dirname(config_file)
+    channel = app.config.get("plugin.slack", "channel")
+    if not channel or channel == "<user>":
+        channel = f"@{getpass.getuser()}"
+    _ = ServiceUpdateMessage(app, obj, repo_folder).send(channel=channel)
+
+class DeployfishMessage(SlackMessage):
+    """A message from deployfish."""
+
+    def __init__(self, app, *args, **kwargs):
+        token = app.config.get("plugin.slack", "token")
+        super().__init__(
+            SlackMessageDivider(),
+            *args,
+            token=token,
+            **kwargs,
+        )
+
+    def add_context(self):
+        self.add_block(
+            SlackMessageContext(
+                SlackMarkdownType(SlackFormatter().datetime()),
+                SlackMarkdownType("Deployfish"),
+            )
+        )
+
+
+class ServiceUpdateMessage(GitChangelogMixin, GitMixin, PythonMixin, DeployfishMessage):
+    """A message indicating that a service has been updated."""
+
+    def __init__(self, app, obj, repo_folder):
+        cwd = os.getcwd()
+        os.chdir(repo_folder)
+        super().__init__(
+            app,
+            SlackMessageHeader(text="Service Update Succeeded"),
+            text="The service has been updated.",
+        )
+        self.values = {}
+        self.annotate(self.values)
+        os.chdir(cwd)
+
+        self.add_service_update(obj)
+        self.add_changelog()
+        self.add_context()
+
+    def add_service_update(self, obj):
+        environment = obj.deployfish_environment
+        block = SlackLabelValueListBlock()
+        block.add_entry(
+            SlackLabelValuePair(
+                label=self.values["name"],
+                value="service updated",
+                label_url=self.url_patterns["repo"],
+            )
+        )
+        block.add_entry(
+            SlackLabelValuePair(
+                label="Environment",
+                value=environment,
+            )
+        )
+        block.add_entry(
+            SlackLabelValuePair(
+                label="Cluster",
+                value=obj.cluster.pk,
+            )
+        )
+        block.add_entry(
+            SlackLabelValuePair(
+                label="Committer",
+                value=self.values["committer"],
+            )
+        )
+        block.add_entry(
+            SlackLabelValuePair(
+                label="Authors",
+                value=",".join(self.values["authors"]),
+            )
+        )
+        self.add_block(block)
+
+    def add_changelog(self):
+        changelog = self.values.get("changelog", [])
+        url = "https://ads-utils-icons.s3.us-west-2.amazonaws.com/ads_dev_ops/database-check.png"
+        text = "*Changelog:*\n"
+        text += str.join("\n", changelog)
+        if text:
+            self.add_block(
+                SlackMessageMarkdown(
+                    text=text,
+                    image_url=url,
+                    alt_text="Changelog",
+                )
+            )
