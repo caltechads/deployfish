5c59c1694ade351dfcbae131baf9b947c7ec528f
Glenn Bach <glenn@caltech.edu>
2024-08-27 13:21:14 -0700

    Bump version: 1.13.0 → 1.13.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,8 +59,8 @@ author: str = 'Chris Malek, Glenn Bach'
 
 show_authors = False
 
-version: str = '1.13.0'
-release: str = '1.13.0'
+version: str = '1.13.1'
+release: str = '1.13.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
